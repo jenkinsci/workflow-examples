4bd721a7a3ab65eacb3028048265103700a0fc4c
ajinkyamhasrup <ajinkya.mhasrup@gmail.com>
2025-02-10 20:18:14 +0530

    Update environmentInStage.groovy

--- declarative-examples/simple-examples/environmentInStage.groovy
+++ declarative-examples/simple-examples/environmentInStage.groovy
@@ -34,11 +34,6 @@ pipeline {
         sh 'echo "FOO is $FOO and BAR is $BAR"'
       }
     }
-
-      stage("timepass") {
-          steps {
-              sh 'echo "FOO is $FOO a*gdgkbdgbs $BAR"'
-          }
       }
 
   }
